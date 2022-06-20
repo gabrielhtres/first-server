68b760f6a139f95010a729e26c778ce0fb19fb81
Gabriel <gabrielhtres@gmail.com>
2022-06-20 17:46:23 -0300

    Alterações

--- index.js
+++ index.js
@@ -4,20 +4,20 @@ const express = require('express')
 var app = express()
 
 // app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.urlencoded({ extended: false }));
+// app.use(express.urlencoded({ extended: false }));
 
 const configCors = function(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'content-type');
   res.setHeader('Content-Type', 'application/json');
-  res.setHeader('Access-Control-Allow-Credentials', true);
+//   res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 };
 
 app.options('*', configCors);
 // app.use(bodyParser.json());
-app.use(express.json());
+// app.use(express.json());
 
 http.createServer(app.get('/', function(req, res) {
     res.send({
