e30c8c44ca9651b1a552107c26bdeb755c22bdaa
Gabriel <gabrielhtres@gmail.com>
2022-06-20 17:41:01 -0300

    Altaração

--- index.js
+++ index.js
@@ -3,6 +3,22 @@ const express = require('express')
 
 var app = express()
 
+// app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }));
+
+const configCors = function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'content-type');
+  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+};
+
+app.options('*', configCors);
+// app.use(bodyParser.json());
+app.use(express.json());
+
 http.createServer(app.get('/', function(req, res) {
     res.send({
         message: 'Root'
